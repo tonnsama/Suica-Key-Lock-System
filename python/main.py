c8f0382d2e6b2497b0d6c1cd925541fd60418acc
kapibarasann <kaznari.2915@gmail.com>
2019-12-23 16:55:59 +0900

    add log

--- python/main.py
+++ python/main.py
@@ -44,6 +44,7 @@ def main():
 				logfile = open(filename_log, mode="a")
 			else:
 				logfile = open(filename_log, mode="w")
+				logfile.write(today + "\n")
 
 
 			target_res = clf.sense(target_req, iterations=10, interval=0.01)
@@ -66,30 +67,35 @@ def main():
 
 				# if it is an Auto Close Card and Key is Locked
 				if is_auto_card:
-					s = "Auto Close Card"
+					s = "Auto Close Card\n"
 					print(s)
 					logfile.write(s)
 
 				if key_state:
-					s = "Current key state is Locked"
+					s = "Current key state is Locked\n"
 					print(s)
 					logfile.write(s)
 				else:
-					s = "Current key state is Unlocked"
+					s = "Current key state is Unlocked\n"
 					print(s)
 					logfile.write(s)
 
 				if is_auto_card and key_state:
-					s = "Express Card : the Key will be Locked Automatically"
+					s = "Express Card : the Key will be Locked Automatically\n"
 					print(s)
 					logfile.write(s)
 
 					unlock()
-					print("Unlocked the key")
+					s = "Unlocked the key\n"
+					print(s)
+					logfile.write(s)
 
 					start_time = time.time()
 					# Waiting for Opening the Door
-					print("Open the door")
+					s = "Open the door\n"
+					print(s)
+					logfile.write(s)
+
 					time.sleep(2)
 					while True:
 						distance = measure()
@@ -99,7 +105,10 @@ def main():
 							break
 						time.sleep(0.1)
 
-					print("Close the door")
+					s = "Close the door\n"
+					print(s)
+					logfile.write(s)
+
 					time.sleep(2)
 					# Winting for Closing the Door
 					while True:
@@ -109,7 +118,9 @@ def main():
 						if distance < CLOSE_DISTANCE or elapsed_time > CLOSE_TIME:
 							time.sleep(1)
 							lock()
-							print("Locked the key")
+							s = "Locked the key\n"
+							print(s)
+							logfile.write(s)
 							break
 						time.sleep(0.1)
 
@@ -124,21 +135,29 @@ def main():
 					for line in normal_cards:
 						# if the Card is Registered
 						if card == line:
-							print("Successiful")
+							s = "Successiful\n"
+							print(s)
+							logfile.write(s)
 							auth = True
 							if key_state: # if Locked
 								unlock()
-								print("Unlocked the key")
+								s = "Unlocked the key\n"
+								print(s)
+								logfile.write(s)
 								key_state = False
 							else:	      # if Unlocked
 								lock()
-								print("Locked the key")
+								s = "Locked the key\n"
+								print(s)
+								logfile.write(s)
 								key_state = True
 							break
 
 				# if the card is WRONG
 				if not auth:
-					print("Wrong Card")
+					s = "Wrong Card\n"
+					print(s)
+					logfile.write(s)
 					wrongCard(key_state)
 
 				normal_cards.close()
@@ -148,14 +167,21 @@ def main():
 
 		except KeyboardInterrupt:
 			pass
-			print("exit: Keyboard Intterrupt")
+			s = "exit: Keyboard Intterrupt\n"
+			print(s)
+			logfile.write(s)
+
 			lock()
 			os.system("sudo killall servod")
 			break
 
+
 		except IOError:
 			pass
-			print("exit: IO Error")
+			s ="exit: IO Error\n"
+			print(s)
+			logfile.write(s)
+
 			lock()
 			os.system("sudo killall servod")
 			break
