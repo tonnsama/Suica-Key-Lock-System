14ac42251fa1acaf3c93a6970729d2ffca5496fd
kapibarasann <kaznari.2915@gmail.com>
2020-05-04 21:26:22 +0900

    reed switch gpiozero

--- python/switch.py
+++ python/switch.py
@@ -2,26 +2,36 @@
 
 import RPi.GPIO as GPIO
 import time
-from sensor import *
+# from keymodule.sensor import *
+from gpiozero import DigitalInputDevice as ReedSwitch
 
-# sw_pin = 27
+reed_switch = ReedSwitch(27)
 
 # GPIO.setmode(GPIO.BCM)
-# GPIO.setup(sw_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# reed_sw = 27
+# GPIO.setup(reed_sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# def isClosed():
+# 	if GPIO.input(reed_sw) == GPIO.HIGH:
+# 		return True
+# 	else:
+# 		return False
+
+def isClosed():
+  return reed_switch.value
+
 
 try:
   while True:
-    # if GPIO.input(sw_pin) == GPIO.HIGH:
-    #   print('Switch ON')
-    # else:
-    #   print('Switch OFF')
-    # time.sleep(0.05)
-    if is_closed():
+
+    if isClosed():
       print('CLOSED')
     else:
       print('OPEN')
+    # print(reed_switch.value)
+
     time.sleep(0.05)
 
 except KeyboardInterrupt:
   pass
-  GPIO.cleanup()
+  # GPIO.cleanup()
